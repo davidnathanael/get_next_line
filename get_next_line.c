39344178fb4f6fdeacb5414785bbfc3e44f651c4
David Nathanael <ddela-cr@student.42.fr>
2015-12-18 00:58:06 +0100

    implemented list for multi buffers

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,14 @@
 /*   By: ddela-cr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/12 22:12:58 by ddela-cr          #+#    #+#             */
-/*   Updated: 2015/12/17 01:18:03 by ddela-cr         ###   ########.fr       */
+/*   Updated: 2015/12/18 00:57:32 by ddela-cr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/uio.h>
 #include "libft/libft.h"
@@ -20,27 +21,29 @@
 
 int			get_next_line(int const fd, char **line)
 {
-	static char		*tmp[MAX_FD];
-	int				ret;
+	static t_list		*list;
+	t_list				*tmp;
+	int					ret;
 
 	if (fd < 0 || !line)
 		return (-1);
+	ft_check_fd(&list, &tmp, fd);
 	ret = BUFF_SIZE;
-	while (ret > 0 || ft_strlen(tmp[fd]))
+	while (ret > 0 || ft_strlen(tmp->overflow))
 	{
-		if ((ft_store_line(&tmp[fd], line)) == 1)
+		if ((ft_store_line(&(tmp->overflow), line)) == 1)
 			return (1);
-		if (ft_append(fd, &tmp[fd], &ret) == -1)
+		if (ft_append(fd, &(tmp->overflow), &ret) == -1)
 			return (-1);
-		if (ret == 0 && ft_strlen(tmp[fd]))
+		if (ret == 0 && ft_strlen(tmp->overflow))
 		{
-			*line = ft_strdup(tmp[fd]);
-			ft_bzero(tmp[fd], ft_strlen(tmp[fd]));
+			*line = ft_strdup(tmp->overflow);
+			ft_bzero(tmp->overflow, ft_strlen(tmp->overflow));
 			return (1);
 		}
 	}
-	free(tmp[fd]);
-	tmp[fd] = NULL;
+	free(tmp->overflow);
+	tmp->overflow = NULL;
 	return (0);
 }
 
@@ -71,4 +74,25 @@ int			ft_append(int const fd, char **tmp, int *ret)
 	*tmp = ft_strjoin(*tmp, buf);
 	free(buf);
 	return (0);
+}
+
+void		ft_check_fd(t_list **list, t_list **tmp, int fd)
+{
+	*tmp = *list;
+	while (*tmp)
+	{
+		if ((*tmp)->fd == fd)
+			break ;
+		*tmp = (*tmp)->next;
+	}
+	if (!*tmp)
+	{
+		t_list		*new;
+
+		new = (t_list *)malloc(sizeof(t_list));
+		new->fd = fd;
+		new->next = *list;
+		*list = new;
+		*tmp = *list;
+	}
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,25 @@
 /*   By: ddela-cr <ddela-cr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/12 22:13:20 by ddela-cr          #+#    #+#             */
-/*   Updated: 2015/12/17 01:15:06 by ddela-cr         ###   ########.fr       */
+/*   Updated: 2015/12/18 00:57:33 by ddela-cr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 9999
-# define MAX_FD 1028
+# define BUFF_SIZE 1
 
-int		get_next_line(int const fd, char **line);
-int		ft_store_line(char **tmp, char **line);
-int		ft_append(int const fd, char **tmp, int *ret);
+typedef struct		s_list
+{
+	int				fd;
+	char			*overflow;
+	struct s_list	*next;
+}					t_list;
+
+int					get_next_line(int const fd, char **line);
+int					ft_store_line(char **tmp, char **line);
+int					ft_append(int const fd, char **tmp, int *ret);
+void				ft_check_fd(t_list **list, t_list **tmp, int fd);
 
 #endif
